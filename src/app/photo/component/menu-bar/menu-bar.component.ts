bc6dc4f356bb6c7ff82bcbd2e55b9e1dec7968a9
moAllElite <mouhamedn347@gmail.com>
2025-05-06 14:02:43 +0000

    add loader on fetch random image

--- src/app/photo/component/menu-bar/menu-bar.component.ts
+++ src/app/photo/component/menu-bar/menu-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, DestroyRef, inject, OnInit, signal, WritableSignal } from '@angular/core';
+import { Component, computed, DestroyRef, inject, OnInit, signal, WritableSignal } from '@angular/core';
 import { MenubarModule } from 'primeng/menubar';
 import { AvatarModule } from 'primeng/avatar';
 import { BadgeModule } from 'primeng/badge';
@@ -6,17 +6,20 @@ import { CommonModule } from '@angular/common';
 import { InputTextModule } from 'primeng/inputtext';
 import { Ripple } from 'primeng/ripple';
 import { Router } from '@angular/router';
-import { map, Subscription } from 'rxjs';
+import {  map, Subscription } from 'rxjs';
+import { MessageService } from 'primeng/api';
 import { Photo } from '../../models/photo.model';
+import { Toast, ToastModule } from 'primeng/toast';
 import { PhotosService } from '../../service/photos.service';
-import { environment } from '../../../../environments/environment.development';
+import { environment } from '../../../../environments/environment.development';import { HttpErrorResponse } from '@angular/common/http';
 import { rxResource } from '@angular/core/rxjs-interop';
 import { LoadingSpinnerComponent } from "../../../shared/components/loading-spinner/loading-spinner.component";
-@Component({
+ @Component({
   selector: 'app-menu-bar',
-  imports: [MenubarModule, AvatarModule, BadgeModule, InputTextModule, Ripple, CommonModule],
+  imports: [MenubarModule, AvatarModule, ToastModule, BadgeModule, InputTextModule, Ripple, CommonModule, LoadingSpinnerComponent],
   templateUrl: './menu-bar.component.html',
-  styleUrl: './menu-bar.component.scss'
+  styleUrl: './menu-bar.component.scss',
+  providers:[MessageService]
 })
 export class MenuBarComponent implements OnInit {
 
@@ -27,12 +30,13 @@ export class MenuBarComponent implements OnInit {
   client_id:string = environment.clientId; // client id for unsplash api
   photo$ !:Subscription;
   destroy = inject(DestroyRef); // inject destroy ref for unsubscribing from observables
+  private messageService: MessageService = inject(MessageService);//inject messageService for toast display
 
 
   //navigate on page
   navigateTo(item: MenuItem) {
     if(item.label.includes('aléatoire')) {
-      this.getRandomPhoto(); 
+
       setTimeout(() => {
         this.router.navigate([item.routeLink,`${this.photoId()}`]);
       },500);
@@ -44,15 +48,35 @@ export class MenuBarComponent implements OnInit {
   }
   photoId :WritableSignal<string> = signal('');
 
-  
+
+  randomPhoto = rxResource({
+    request:()=> this.photoId(),
+    loader:({request})=> this.photoService.getRandomPhoto(this.client_id)
+    .pipe(
+      map((data)=>{
+        this.photoId.set(data.id);
+      })
+    )
+  });
+
 
   //fetch random photo from unsplash api
    getRandomPhoto() {
     this.photo$ = this.photoService.getRandomPhoto(this.client_id).subscribe({
-      
+      next: (data) =>{
+        this.photoId.set(data.id);
+      },
+      error:(err:HttpErrorResponse)=> this.showError(err.error)
+
     }    );
     this.destroy.onDestroy(() => this.photo$.unsubscribe());
   }
+  // show error toast
+  showError(message:string) {
+
+    this.messageService.add({ severity: 'error', summary: 'Error', detail: message });
+}
+
   ngOnInit() {
     this.items = [
         {
